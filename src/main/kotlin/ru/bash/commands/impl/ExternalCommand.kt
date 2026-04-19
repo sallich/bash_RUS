88417e12d83d68044f7cb6db252fcfa1828da975
ruslansadykov <ruslansadykov2013@yandex.ru>
2026-04-20 01:57:52 +0300

    fix stdin/stdout with async + add redirects

--- src/main/kotlin/ru/bash/commands/impl/ExternalCommand.kt
+++ src/main/kotlin/ru/bash/commands/impl/ExternalCommand.kt
@@ -1,5 +1,9 @@
 package ru.bash.commands.impl
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import ru.bash.commands.Command
 import java.io.InputStream
 import java.io.OutputStream
@@ -8,14 +12,10 @@ class ExternalCommand(
     override val name: String
 ) : Command {
 
-    private fun fixDottedCommand(argv: List<String>): List<String> {
-        val result = when {
-            argv.isEmpty() -> argv
-            argv[0] == "./" && argv.size > 1 ->
-                listOf("./" + argv[1]) + argv.drop(2)
-            else -> argv
-        }
-        return result
+    private fun fixDottedCommand(argv: List<String>): List<String> = when {
+        argv.isEmpty() -> argv
+        argv[0] == "./" && argv.size > 1 -> listOf("./" + argv[1]) + argv.drop(2)
+        else -> argv
     }
 
     override fun execute(argv: List<String>, stdin: InputStream, stdout: OutputStream, stderr: OutputStream): Int {
@@ -23,31 +23,21 @@ class ExternalCommand(
         val inheritStdin = stdin === System.`in`
 
         val builder = ProcessBuilder(fixedArgv)
-        if (inheritStdin) {
-            builder.redirectInput(ProcessBuilder.Redirect.INHERIT)
-        }
+        if (inheritStdin) builder.redirectInput(ProcessBuilder.Redirect.INHERIT)
         val process = builder.start()
 
-        val pipeIn = if (!inheritStdin) Thread {
-            try {
-                stdin.copyTo(process.outputStream)
-            } finally {
-                process.outputStream.close()
+        return runBlocking(Dispatchers.IO) {
+            val pipes = buildList {
+                if (!inheritStdin) add(async {
+                    try { stdin.copyTo(process.outputStream) }
+                    finally { process.outputStream.close() }
+                })
+                add(async { process.inputStream.copyTo(stdout) })
+                add(async { process.errorStream.copyTo(stderr) })
             }
-        } else null
-
-        val pipeOut = Thread { process.inputStream.copyTo(stdout) }
-        val pipeErr = Thread { process.errorStream.copyTo(stderr) }
-
-        pipeIn?.start()
-        pipeOut.start()
-        pipeErr.start()
-
-        val exitCode = process.waitFor()
-        pipeIn?.join()
-        pipeOut.join()
-        pipeErr.join()
-
-        return exitCode
+            val exitCode = async { process.waitFor() }
+            pipes.awaitAll()
+            exitCode.await()
+        }
     }
 }
